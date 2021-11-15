9a502fbfe2957e8be7a6d199e01d10685113db45
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-15 15:36:59 +0300

    created feedbackstats component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import FeedbackList from './components/FeedbackList.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
 
 	let feedback = [
 		{
@@ -19,6 +20,9 @@
 		},
 	]
 
+	$: count = feedback.length
+	$: average = feedback.reduce((a, { rating }) => a + rating, 0) / feedback.length
+
 	const deleteFeedback = (e) => {
 		const itemId = e.detail
 		feedback = feedback.filter((item) => item.id !== itemId)
@@ -26,6 +30,7 @@
 </script>
 
 <main class="container">
+	<FeedbackStats {count} {average} />
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
