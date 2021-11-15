40c1f56ba3c3325f641830dfda02adbb9a7aff3e
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-15 15:16:21 +0300

    adding components and styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,29 @@
 <script>
-	let firstName = 'Konstantin';
-	let lastName = 'Krumin';
-	let color = 'blue';
-	let showText = false;
-	let users = [
+	import FeedbackList from './components/FeedbackList.svelte';
+
+	let feedback = [
 		{
-			id: '1',
-			name: 'John'
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
 		},
 		{
-			id: '2',
-			name: 'Sarah'
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
 		},
 		{
-			id: '3',
-			name: 'Bob'
-		}
-	];
-
-	$: name = firstName + ' ' + lastName;
-
-	const toggle = () => {
-		color = color === 'blue' ? 'orange' : 'blue';
-		showText = !showText;
-		users = [...users, { id: '4', name: 'Jen' }]
-	}
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+		},
+	]
 </script>
 
-<main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	{:else}
-		<p>No Text</p>
-	{/if}
-
-	<button on:click={toggle}>Click</button>
-
-	{#each users as user (user.id)}
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
+<main class="container">
+	<FeedbackList {feedback} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
